d8fc03bd0b07980e3ebf734eadd8167c5ec0d408
Charles Davenport <cdavenport090@gmail.com>
2019-07-24 19:55:46 -0400

    Populated cnn, layer tables; added a saved model binary

---
+++ src/sql/insert_cnn.sql
@@ -0,0 +1,20 @@
+INSERT INTO cnn (id, name, layers, trainable_params, file_path)
+VALUES (001, "MNIST_CNN_01.h5", 7, 225034, "\\store\\saved_models\\MNIST_CNN_01.h5" );
+
+#UPDATE cnn SET file_path="\\store\\saved_models\\MNIST_CNN_01.h5" WHERE id=001;
+
+SELECT * FROM cnn JOIN layer;
+
+# these values are from model.summary() - or model.json()
+INSERT INTO layer(cnn_id, depth, type, params, kernel_x, kernel_y, stride_x, stride_y)
+VALUES (001, 1,  'CONV_2D', 320, 3,3, 1,1);
+INSERT INTO layer(cnn_id, depth, type, params, kernel_x, kernel_y, stride_x, stride_y)
+VALUES (001, 2,  'POOL', 0, 2,2, 1,1);
+INSERT INTO layer(cnn_id, depth, type, params, kernel_x, kernel_y, stride_x, stride_y)
+VALUES (001, 3,  'CONV_2D', 18496, 3,3, 1,1);
+INSERT INTO layer(cnn_id, depth, type, params, kernel_x, kernel_y, stride_x, stride_y)
+VALUES (001, 4,  'POOL', 0, 2,2, 1,1);
+INSERT INTO layer(cnn_id, depth, type, params, kernel_x, kernel_y, stride_x, stride_y)
+VALUES (001, 5,  'DENSE', 204928, null, null, null, null);
+INSERT INTO layer(cnn_id, depth, type, params, kernel_x, kernel_y, stride_x, stride_y)
+VALUES (001, 6,  'DENSE', 1290, null, null, null, null);
