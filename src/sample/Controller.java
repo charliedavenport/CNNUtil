c890e3f81bc08258ef64ddc0aa0418fab9b04464
AnonymousFool <qsw66793@uga.edu>
2019-07-29 00:39:06 -0400

    Added line chart functionality

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
@@ -16,6 +19,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
+
+    private class refHolder {
+        public String modelName;
+        public Series loss;
+        public Series acc;
+
+        public refHolder(String _modelName, Series _loss, Series _acc) {
+            modelName = _modelName;
+            loss = _loss;
+            acc = _acc;
+        }
+    }
 
     @FXML
     private ComboBox<String> modelComboBox;
@@ -32,9 +47,18 @@ public class Controller {
     @FXML
     private ComboBox<String> statsModelComboBox;
 
+    @FXML
+    private LineChart<Number, Number> lossChart;
+
+    @FXML
+    private LineChart<Number, Number> accChart;
+
     private String currentModelName;
     private String currentDatasetName;
     private String statsDatasetName;
+    private String statsModelName;
+
+    private ArrayList<refHolder> lineChartList = new ArrayList<>();
 
     public Controller() {
 
@@ -56,6 +80,8 @@ public class Controller {
         modelComboBox.valueProperty().addListener((observableValue, s, t1) -> updateModelData());
         statsDatasetComboBox.valueProperty()
                 .addListener((ov, s, tl) -> statsDatasetName = tl);
+        statsModelComboBox.valueProperty()
+                .addListener((ov, s, tl) -> statsModelName = tl);
     }
 
     @FXML
@@ -94,7 +120,46 @@ public class Controller {
         List<String> modelNames = TestQuery.SelectCnnNames(statsDatasetName);
         statsModelComboBox.getItems().setAll(modelNames.toArray(new String[0]));
         statsModelComboBox.setVisible(true);
+        lossChart.setVisible(false);
+        lossChart.getData().clear();
+        accChart.setVisible(false);
+        accChart.getData().clear();
     }
 
+    @FXML
+    private void handleStatModelCombo(ActionEvent event) {
+        refHolder statsRef = null;
+        for(refHolder reference : lineChartList)
+            if(reference.modelName.equals(statsModelName)) {
+                statsRef = reference;
+                break;
+            }
+        if(statsRef != null) {
+            lossChart.getData().remove(statsRef.loss);
+            accChart.getData().remove(statsRef.acc);
+            lineChartList.remove(statsRef);
+            return;
+        }
+        Series lossSeries = new Series();
+        Series accSeries = new Series();
+
+        List<List<Number>> lists = TestQuery.SelectLossAccuracy(statsDatasetName, statsModelName);
+        for(int i = 0; i < lists.get(0).size(); i++) {
+            System.out.println(lists.get(1).get(i));
+            lossSeries.getData().add(new Data(lists.get(0).get(i), lists.get(1).get(i)));
+            accSeries.getData().add(new Data(lists.get(0).get(i), lists.get(2).get(i)));
+        }
+
+        lossSeries.setName(statsModelName);
+        accSeries.setName(statsModelName);
+
+        lossChart.getData().add(lossSeries);
+        accChart.getData().add(accSeries);
+        lossChart.setVisible(true);
+        accChart.setVisible(true);
+
+        statsRef = new refHolder(statsModelName, lossSeries, accSeries);
+        lineChartList.add(statsRef);
+    }
 
 }
--- src/sample/TestQuery.java
+++ src/sample/TestQuery.java
@@ -131,7 +131,6 @@ public class TestQuery {
             stmt.execute();
             ResultSet rs = stmt.getResultSet();
             rs.first();
-            System.out.println(rs.getString(1));
             do {
                 names.add(rs.getString(1));
             } while (rs.next());
@@ -144,6 +143,49 @@ public class TestQuery {
     }
 
     /**
+     * @param dataset   the dataset on which cnn's should have been trained
+     * @param cnn       the cnn whose training data should be loaded
+     * @author Quinn Wyner
+     * @return - lists of epochs, losses, and accuracies for training cnn on dataset
+     */
+    public static List<List<Number>> SelectLossAccuracy(String dataset, String cnn) {
+        final String query = "SELECT T.epoch, T.loss, T.acc\n" +
+                "FROM train T,\n" +
+                "\t(SELECT D.id\n" +
+                "     FROM dataset D\n" +
+                "     WHERE D.name = ?) E,\n" +
+                "\t(SELECT C.id\n" +
+                "     FROM cnn C\n" +
+                "     WHERE C.name = ?) N\n" +
+                " WHERE T.cnn_id = N.id\n" +
+                "   AND T.data_id = E.id\n" +
+                "ORDER BY T.epoch;";
+        List<List<Number>> lists = new ArrayList<>();
+        for(int i = 0; i < 3; i++)
+            lists.add(new ArrayList<>());
+        try {
+            connectToDB();
+
+            CallableStatement stmt = conn.prepareCall(query);
+            stmt.setString(1, dataset);
+            stmt.setString(2, cnn);
+            stmt.execute();
+            ResultSet rs = stmt.getResultSet();
+            rs.first();
+            do {
+                lists.get(0).add(rs.getInt("epoch"));
+                lists.get(1).add(rs.getDouble("loss"));
+                lists.get(2).add(rs.getDouble("acc"));
+            } while (rs.next());
+        }
+        catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return lists;
+    }
+
+    /**
      * Connect to mysql database.
      * @author Charles Davenport
      */
@@ -152,8 +194,8 @@ public class TestQuery {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             Properties prop = new Properties();
-            prop.put("user", "java");
-            prop.put("password", "admin");
+            prop.put("user", "root");
+            prop.put("password", "16FishFillet");
             prop.put("serverTimezone", "UTC");
             prop.put("allowMultiQueries", true);
             conn = DriverManager.getConnection(url, prop);
