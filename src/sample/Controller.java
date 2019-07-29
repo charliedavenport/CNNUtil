cf2a6ea8c03105c4ca112d03aa0d2550e441bf5a
Stoniel <stone.daniel416@gmail.com>
2019-07-28 21:14:31 -0400

    git ignore

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,11 +2,16 @@ package sample;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +45,24 @@ public class Controller {
                 .addListener((ov, s, t1) -> currentDatasetName = t1);
         modelComboBox.valueProperty()
                 .addListener((observableValue, s, t1) -> currentModelName = t1);
+        modelComboBox.valueProperty().addListener((observableValue, s, t1) -> updateModelData());
+    }
+
+    @FXML
+    private void updateModelData(){
+        try {
+            modelDataListView.getItems().clear();
+            ResultSet rs = TestQuery.selectCNNByName(currentModelName);
+            ResultSetMetaData meta = rs.getMetaData();
+            int numCols = meta.getColumnCount();
+            rs.first();
+            for (int i = 1; i <= numCols; i++) {
+                modelDataListView.getItems().add(rs.getString(i) + " " + meta.getColumnName(i));
+            }
+
+        }catch (SQLException ex) {
+
+        }
     }
 
     @FXML
