031bbc4f72f866600faa5a08a8a7ec7684afd731
Charles Davenport <cdavenport090@gmail.com>
2019-07-28 23:40:40 -0400

    Merge branch 'master' of https://github.com/charliedavenport/CNNUtil
    # Conflicts:
    #	out/production/CNNUtil/sample/Main.class
    #	src/sample/Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,10 @@
 package sample;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
@@ -42,10 +34,10 @@ public class Controller {
 
     @FXML
     private void initialize() {
-        List<String> modelNames = TestQuery.SelectCnnNames();
+        List<String> modelNames = DBAccess.SelectCnnNames();
         modelComboBox.getItems().setAll(modelNames.toArray(new String[0]));
 
-        List<String> datasetNames = TestQuery.SelectDatasetNames();
+        List<String> datasetNames = DBAccess.SelectDatasetNames();
         datasetComboBox.getItems().setAll(datasetNames.toArray(new String[0]));
         statsDatasetComboBox.getItems().setAll(datasetNames.toArray(new String[0]));
 
@@ -60,38 +52,16 @@ public class Controller {
 
     @FXML
     private void updateModelData(){
-        try {
-            modelDataListView.getItems().clear();
-            ResultSet rs = TestQuery.selectCNNByName(currentModelName);
-            ResultSetMetaData meta = rs.getMetaData();
-            int numCols = meta.getColumnCount();
-            rs.first();
-            modelDataListView.getItems().add(rs.getString(1) + " " + meta.getColumnName(1));
-            modelDataListView.getItems().add(rs.getString(2) + " " + meta.getColumnName(2));
-            do {
-                String temp = "";
-                for (int i = 3; i <= numCols; i++) {
-                    temp += meta.getColumnName(i) + " " + rs.getString(i) + "\t";
-                }
-                modelDataListView.getItems().add(temp);
-            } while (rs.next());
-//            for (int i = 1; i <= numCols; i++) {
-//                modelDataListView.getItems().add(rs.getString(i) + " " + meta.getColumnName(i));
-//            }
 
-        }catch (SQLException ex) {
+        modelDataListView.getItems().clear();
+        List<String> modelData = DBAccess.CNNInfoByName(currentModelName);
 
-        }
-    }
-
-    @FXML
-    private void handleDatasetCombo(ActionEvent event) {
-
+        modelDataListView.getItems().addAll(modelData);
     }
 
     @FXML
     private void handleStatDatasetCombo(ActionEvent event) {
-        List<String> modelNames = TestQuery.SelectCnnNames(statsDatasetName);
+        List<String> modelNames = DBAccess.SelectCnnNames(statsDatasetName);
         statsModelComboBox.getItems().setAll(modelNames.toArray(new String[0]));
         statsModelComboBox.setVisible(true);
     }
